427cd6f17c12b8a7e299f3294acc99d28ef94032
noubar <noubar.kaloust@gmail.com>
2021-07-18 18:37:47 +0400

    Added Cards To The Arr

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -51,23 +51,24 @@ export const INFO: Info[] = [{
 
   },
   {
-    title: "Buy Grocery",
-    imageUrl: "assets/healthy-food.jpg",
-    description: "Make sure to pick up healthy food",
+    title: "Do Some Lunges",
+    imageUrl: "assets/lunges.jpg",
+    description: "At Least Till You Die!",
     dueDate: "20/07/2021"
 
   },
   {
-    title: "Go for a Walk",
-    imageUrl: "assets/walk.jpg",
-    description: "At least for 30 KM",
-    dueDate: "30/07/2021"
+    title: "RIP",
+    imageUrl: "assets/RIP.jpg",
+    description: "Say Hello To Your Uncle",
+    dueDate: "26/08/2023"
 
   },
   {
-    title: "Learn Angular",
-    imageUrl: "assets/angular.jpg",
-    description: "Find free courses",
-    dueDate: "26/07/2021"
+    title: "New Chapter",
+    imageUrl: "assets/heaven.jpeg",
+    description: "Make Sure Not To Fall Down Again ",
+    dueDate: "30/07/2021"
 
-  }]
+  }
+]
